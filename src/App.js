b9b4857e8f517367494310042c30e1033eced83f
Yaron Karlinski <skarlinski@gmail.com>
2021-01-10 13:42:42 +0200

    Add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
-
+import Container from 'react-bootstrap/Container';
 import './App.css';
 import CarsComp from './components/CarsView/CarsView';
 import CarModel from './data-models/CarModel';
 
 function App() {
-  
+  const carsData = [];
+  carsData.push(new CarModel("Toyota", "Yaris", 2002, 230000));
+  carsData.push(new CarModel("Toyota", "Corola", 2015, 105000));
+  carsData.push(new CarModel("Hyundai", "i30", 2010, 150000));
   return (
-    <div>
-      <CarsComp/>
-    </div>
+    <Container>
+      <CarsComp cars={carsData}/>
+    </Container>
   );
 }
 
--- src/components/CarsView/CarsView.js
+++ src/components/CarsView/CarsView.js
@@ -1,24 +1,39 @@
 import React from 'react';
-
+import Table from 'react-bootstrap/Table';
 
 class CarsComp extends React.Component{
     constructor(props) {
         super(props);
+        // our Car model constructor(brand, model, year, km)
     } 
     render(){
        const carTableRows = [];
-       return ( <table class="table">
+       console.log(this.props.cars);
+       for(let i=0; i<this.props.cars.length; i++) {
+           const rowContent = <tr>
+               <td>{this.props.cars[i].brand}</td>
+               <td>{this.props.cars[i]['model']}</td>
+               <td>{this.props.cars[i]['km']}</td>
+               <td>{this.props.cars[i]['year']}</td>
+               <td>{this.props.cars[i].kmPerYear()}</td>
+               </tr> 
+           carTableRows.push(rowContent);
+       }
+       return ( 
+       <Table striped hover>
         <thead>
-            <th>Brand</th>
-            <th>Model</th>
-            <th>KM</th>
-            <th>Year</th>
-            <th>Km Per Year</th>
+            <tr>
+                <th>Brand</th>
+                <th>Model</th>
+                <th>KM</th>
+                <th>Year</th>
+                <th>Km Per Year</th>
+            </tr>
         </thead>
         <tbody>
             {carTableRows}
         </tbody>
-    </table>)
+    </Table>)
     }
 }
 
