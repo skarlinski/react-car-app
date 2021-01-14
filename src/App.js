a74e8cfba874ce3392140fc34b85be5aba4ae7c2
Yaron Karlinski <skarlinski@gmail.com>
2021-01-14 11:04:14 +0200

    Add basic routing

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+/// npm install -> read package.json, find all dependencies install each one
+// npm install react-router-dom -> 1) Will add to package.json the new dependency. 2) install it in node_modules
+
 import React from 'react';
 import axios from 'axios';
 
@@ -5,6 +8,8 @@ import Container from 'react-bootstrap/Container';
 import './App.css';
 import CarsComp from './components/CarsView/CarsView';
 import CarModel from './data-models/CarModel';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+
 
 class App extends React.Component {  
   constructor(props){
@@ -16,10 +21,31 @@ class App extends React.Component {
   render(){
     // Component will be rendered the data arrives
     // When the data arrive - nothing will happen
+    // 1. Wrap the whole app in HashRouter / BrowserRouter
+    // 2. Add Routes for different urls
+    // 3. Add Switch to make sure only one Route is active
     return (
-      <Container>
-        <CarsComp cars={this.state.carsData}/>
-      </Container> 
+      <HashRouter>
+              <Container>
+                <nav>
+                  <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/#/cars">Cars View</a></li>
+                  </ul>
+                </nav>
+                <Switch>              
+                  <Route path="/cars">
+                    <h1> Welcome to /cars !</h1>
+                    <CarsComp cars={this.state.carsData}/>
+                  </Route>
+
+                  <Route  path="/">
+                        <h1>Welcome Home</h1>
+                        <p> HomePage should only appear in '/' path</p>
+                  </Route>
+                </Switch>
+              </Container>
+      </HashRouter>
     );
   }
   componentDidMount(){
